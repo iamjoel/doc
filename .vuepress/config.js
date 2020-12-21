3ccf5ffc86faa8d7b1610962866e330d6bbf8f1d
金伟强 <jinweiqiang@coding.net>
2020-12-21 14:45:49 +0800

    add doc

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: '常用文档',
+  base: '/doc/', // 部署站点的基础路径
+  dest: './dist', // 构建输出的位置，从项目根路径开始算。
+  themeConfig: {
+    sidebar: [
+      {
+        title: '前端',
+        children: [
+          {
+            title: 'DOM 常用操作',
+            path: '/content/fe/DOM'
+          }
+        ]
+      }
+    ]
+  }
+}
